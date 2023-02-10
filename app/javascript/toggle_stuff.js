1f987212e4086def3e02858642dc43d1fb8f6e13
David Lopez <david@davidclopez.me>
2023-02-10 09:52:47 -0700

    mobile nav

--- app/javascript/toggle_stuff.js
+++ app/javascript/toggle_stuff.js
@@ -1,5 +1,29 @@
-const intro = document.getElementById("intro");
-intro.addEventListener('click', () => {
-    intro.classList.toggle("bg-purple-700")
-    intro.classList.toggle("bg-black")
+const toggle = document.getElementById("nav-menu-toggle");
+const menu = document.getElementById("nav-menu-mobile");
+const bg = document.getElementById("nav-menu-fill");
+const all = document.getElementById("all-da-juice");
+
+toggle.addEventListener('click', () => {
+    menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+    bg.style.display = menu.style.display === "block" ? "none" : "block";
+})
+
+menu.addEventListener('click', () => {
+    menu.style.display = "none";
+    bg.style.display = "none";
+})
+
+bg.addEventListener('click', () => {
+    menu.style.display = "none";
+    bg.style.display = "none";
+})
+
+bg.addEventListener('scroll', () => {
+    menu.style.display = "none";
+    bg.style.display = "none";
+})
+
+bg.addEventListener('touchmove', () => {
+    menu.style.display = "none";
+    bg.style.display = "none";
 })
