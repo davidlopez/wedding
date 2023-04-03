9a831335d8f6714b28484a3b2466332b19d45990
David Lopez <david@davidclopez.me>
2023-04-03 07:58:18 -0600

    Adds logging

--- app/controllers/rsvp_controller.rb
+++ app/controllers/rsvp_controller.rb
@@ -4,11 +4,14 @@ class RsvpController < ApplicationController
     if !@invite || @invite.rsvped
       redirect_to '/invite', alert: @invite ? 'The RSVP for this invite has already been submitted.' : 'Invite not found. Try again.'
     end
+    logger.info "***** Invite #{@invite.id} has been found"
   end
 
   def update
-    Invite.find(params.fetch(:invite)[:id]).update(rsvped: true)
+    invite_id = params.fetch(:invite)[:id]
+    Invite.find(invite_id).update(rsvped: true)
     Rsvp.where(id: params.fetch(:rsvp_ids, []).compact).update_all(attending: true)
+    logger.info "***** Invite #{invite_id} has RSVPed"
     redirect_to '/thanks'
   end
 end
